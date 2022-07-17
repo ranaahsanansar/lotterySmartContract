1942c7ce64d66807c8e8d30eea09eaeaac6dea20
Rana Ahsan Ansar <asn.cs21@gmail.com>
2022-07-17 07:26:00 -0700

    Create function to create the random function to get the Random winner participante

--- Lottery.sol
+++ Lottery.sol
@@ -10,12 +10,22 @@ contract Lottery {
         manager = msg.sender; // This is the owner of the Contract
     }
 
-    receive() external payable{
+    receive() external payable
+    {
+        require(msg.value == 2 ether , "Only 2 Ether is required to Participate!");
         participants.push(payable(msg.sender));
     }
 
     function getBalance() public view returns(uint)
     {
+        require(msg.sender == manager , "Only Manager is allow to check the Balance!");
         return address(this).balance;
+    }
+
+    function randomUser() public view returns(uint)
+    {
+        // This project is for practice not for Main net 
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp , participants.length ))); // Create random nuumber to selct the winner candidate
+
     }
 }
